e1442785a8d50775287eac0fab6f58bbb3b293ef
mmirot <140865875+mmirot@users.noreply.github.com>
2025-05-17 18:10:49 -0700

    Add Toast Notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import StainQCForm from './components/StainQCForm';
 import StainList from './components/StainList';
 import './App.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-right" />
         <header className="App-header">
           <h1>Daily Stain QC</h1>
           <nav className="nav-links">
@@ -22,5 +24,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
