73debb0e8adfcbbcfcff1302db183270cb4734de
mmirot <140865875+mmirot@users.noreply.github.com>
2025-05-27 21:39:11 -0700

    Fix missing styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
